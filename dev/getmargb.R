840ca8cd4383e845e3e6de07fbed145427d9cc90
jamesmurray7 <j.murray7@ncl.ac.uk>
2023-02-22 12:30:51 +0000

    More work with getmargb

--- dev/getmargb.R
+++ dev/getmargb.R
@@ -2,6 +2,7 @@
 get.marg.b <- function(fit, burnin, N, tune){
   if(!inherits(fit, "joint")) stop("Only usable with object of class 'joint'.")
   if(is.null(fit$dmats)) stop("Need dmats.")
+  if(missing(tune)) tune <- 1
   # Unpack dmats
   M <- fit$ModelInfo
   dm <- fit$dmats$long
@@ -26,9 +27,12 @@ get.marg.b <- function(fit, burnin, N, tune){
   beta.inds <- lapply(M$inds$beta, function(x) x-1)
   b.inds <- lapply(M$inds$b, function(x) x-1)
   ff <- M$family
+  nobs <- sapply(lapply(Y, el), length)
   
   # Inits + metropolis scheme inputs
   b <- lapply(1:M$n, function(i) c(fit$REs[i,,drop=F]))
+  Sigmas <- lapply(1:M$n, function(i) attr(fit$REs, 'vcov')[i,])
+  Sigmas <- lapply(Sigmas, vech2mat, q)
   iters <- burnin + N
   out <- vector('list', M$n)
   accepts <- numeric(M$n)
@@ -39,7 +43,8 @@ get.marg.b <- function(fit, burnin, N, tune){
     # Burnin phase (if req.)
     if(burnin > 0){
       for(j in 1:burnin){
-        b.prop <- MASS::mvrnorm(n = 1, mu = b.current, Sigma = D)
+        # b.prop <- MASS::mvrnorm(n = 1, mu = b.current, Sigma = D)
+        b.prop <- MASS::mvrnorm(n = 1, mu = b.current, Sigma = Sigmas[[i]] * tune)
         fyTb.current <- exp(-joint_density(b.current, Y = Y[[i]], X = X[[i]], Z = Z[[i]],
                                           beta = beta, D = D, sigma = sigma, family = ff, 
                                           Delta = Del[[i]], S = S[[i]], Fi = Fi[[i]], l0i = l0i[[i]],
@@ -50,7 +55,7 @@ get.marg.b <- function(fit, burnin, N, tune){
                                            Delta = Del[[i]], S = S[[i]], Fi = Fi[[i]], l0i = l0i[[i]],
                                            SS = SS[[i]], Fu = Fu[[i]],  haz = l0u[[i]], gamma_rep = gamma.rep,
                                            zeta = zeta, beta_inds = beta.inds, b_inds = b.inds, K = K))
-        P <- fyTb.proposl/fyTb.current
+        P <- min(fyTb.proposl/fyTb.current, 1)
         U <- runif(1)
         if(U <= P){
           b.current <- b.prop
@@ -60,7 +65,8 @@ get.marg.b <- function(fit, burnin, N, tune){
     # Metropolis
     accept <- 0
     for(j in 1:N){
-      b.prop <- MASS::mvrnorm(n = 1, mu = b.current, Sigma = D)
+      # b.prop <- MASS::mvrnorm(n = 1, mu = b.current, Sigma = D)
+      b.prop <- MASS::mvrnorm(n = 1, mu = b.current, Sigma = Sigmas[[i]] * tune)
       fyTb.current <- exp(-joint_density(b.current, Y = Y[[i]], X = X[[i]], Z = Z[[i]],
                                          beta = beta, D = D, sigma = sigma, family = ff, 
                                          Delta = Del[[i]], S = S[[i]], Fi = Fi[[i]], l0i = l0i[[i]],
@@ -71,7 +77,7 @@ get.marg.b <- function(fit, burnin, N, tune){
                                          Delta = Del[[i]], S = S[[i]], Fi = Fi[[i]], l0i = l0i[[i]],
                                          SS = SS[[i]], Fu = Fu[[i]],  haz = l0u[[i]], gamma_rep = gamma.rep,
                                          zeta = zeta, beta_inds = beta.inds, b_inds = b.inds, K = K))
-      P <- fyTb.proposl/fyTb.current
+      P <- min(fyTb.proposl/fyTb.current, 1)
       U <- runif(1)
       if(U <= P){
         b.current <- b.prop
@@ -84,9 +90,73 @@ get.marg.b <- function(fit, burnin, N, tune){
     accepts[i] <- accept/N
   }
   close(pb)
-  list(walks = out, acceptance = accepts)
+  out <- list(walks = out, acceptance = accepts, M = M, bhats = do.call(rbind, b),
+              q = q, K = K, qnames = colnames(fit$coeffs$D),
+              burnin = burnin, N = N, tune = tune, nobs = nobs)
+  class(out) <- 'marginal.b.joint'
+  out
 }
 
-# Get `fit` from joint.Rd for example.
-# aad <- get.marg.b(fit, 500, 5000, F)
-# par(mfrow=c(2,2)); for(j in 1:4) plot(density(do.call(rbind, aad$walks)[,j]),main=bquote(b[.(j-1)]));par(mfrow=c(1,1))
+#' @keywords internal
+#' @method print marginal.b.joint
+print.marginal.b.joint <- function(x, ...){
+  if(!inherits(x, 'marginal.b.joint')) stop("x must be a 'marginal.b.joint' object.")
+  # Data information
+  cat("Marginal distribution f(b_i|Y_i,T_i,Delta_i;Omega)\n")
+  cat("------------------------------\n")
+  cat(sprintf("Number of subjects: %d\n", M$n))
+  cat(sprintf("Number of events: %d (%.2f%%)\n", M$nev, 100 * M$nev/M$n))
+  cat(sprintf("Number of responses: %d, dimension of random effects: %d\n", x$K, x$q))
+  nobs <- x$nobs
+  qnobs <- quantile(nobs)
+  if(qnobs[3] == qnobs[2] && qnobs[3] == qnobs[4])
+    cat(sprintf("Median profile length: %d\n", qnobs[3]))
+  else
+    cat(sprintf("Median [IQR] profile length: %d [%d, %d]\n", qnobs[3], qnobs[2], qnobs[4]))
+  
+  # MC Information
+  cat("\n")
+  accepts <- x$acceptance
+  walks <- do.call(rbind, x$walks)
+  colnames(walks) <- x$qnames
+  cat("Metropolis Algorithm information:\n")
+  cat(sprintf("Burn-in: %d, MC iterations post burn-in: %d\n", x$burnin, x$N))
+  cat(sprintf("Median acceptance rate: %.2f%%\n", median(accepts) * 100))
+  
+  # Print summary of the things 
+  cat("\n")
+  cat("Summary: \n")
+  summ <- apply(walks, 2, summary)
+  summ <- rbind(summ[1:4,], Emp.SD = apply(walks, 2, sd), summ[5:6,])
+  print(round(summ, 4))
+  
+  invisible(x)
+}
+
+#' @keywords internal
+#' @method plot marginal.b.joint
+plot.marginal.b.joint <- function(x, D = NULL, nrow = NULL, ncol = NULL, title = NULL, ...){
+  if(!inherits(x, 'marginal.b.joint')) stop("x must be a 'marginal.b.joint' object.")
+  .par <- par(no.readonly = T)
+  walks <- do.call(rbind, x$walks)
+  num.plots <- x$q # Determine number of plots
+  if(is.null(nrow) & !is.null(ncol) | !is.null(nrow) & is.null(ncol)) 
+    stop("Define either both 'nrow' and 'ncol', or leave both unspecified.")
+  if(is.null(nrow) & is.null(ncol)){ # Plot as if intercept and slope by default.
+    nrow <- max(cumsum(unlist(x$M$ind$b)%%2))
+    ncol <- 2
+  }
+  par(mfrow = c(nrow, ncol))
+  for(j in 1:num.plots){
+    dj <- density(walks[,j])
+    plot(dj, xlab = '', main = x$qnames[j])
+    if(!is.null(D)){
+      curve(dnorm(x, mean = 0, sd = sqrt(D[j,j])),
+            from = min(dj$x), to = max(dj$x), n = 1e3, add = T, lty = 3, col = 'red')
+    }
+  }
+  if(!is.null(title))
+    mtext(title, side - 3, line = -2, outer = TRUE)
+  on.exit(par(.par))
+}
+
