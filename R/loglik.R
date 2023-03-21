9178fe7de489309ee7c7f7c5a2c359e8ad54e013
jamesmurray7 <j.murray7@ncl.ac.uk>
2023-03-21 12:31:10 +0000

    Removing example which caused an error.

--- R/loglik.R
+++ R/loglik.R
@@ -214,11 +214,7 @@ extractAIC.joint <- function(fit, scale, k = 2, conditional = FALSE, ...){
 #'              control = list(verbose = FALSE))
 #' fit2 <- joint(long.formulas2, surv.formula, PBC, family, control = list(verbose = FALSE))
 #' anova(fit, fit2)
-#' # Quadratic terms improve fit significantly. Now try cubic spline -------------
-#' long.formulas3 <- list(serBilir ~ drug * splines::ns(time, 3) + (1 + splines::ns(time, 3))|id)
-#' fit3 <- joint(long.formulas3, surv.formula, PBC, family, control = list(verbose = FALSE))
-#' anova(fit2, fit3)
-#' # Spline terms do _not_ improve fit -------------------------------------------
+#' # Quadratic terms improve fit significantly. 
 #' }
 anova.joint <- function(object, object2, ...){
   if(missing(object) || missing(object2))
