a745966230dd15fdfe66a04f83a1bb779cbf0380
jamesmurray7 <j.murray7@ncl.ac.uk>
2023-05-31 11:59:14 +0100

    headaches

---
+++ paper-sims/Application/LongInvestigation.R
@@ -0,0 +1,74 @@
+rm(list=ls())
+data("PBC", package = 'gmvjoint')
+library(glmmTMB)
+PBC$AST <- log(PBC$SGOT)
+# PBC$prothrombin <- (PBC$prothrombin * .1)^(-4)
+
+
+# Binary ------------------------------------------------------------------
+# No matter what, we fit binary with intercept only RE.
+
+# Counts ------------------------------------------------------------------
+# Platelets
+P1 <- glmmTMB(platelets ~ drug * time + (1 + time|id), PBC, poisson)
+VarCorr(P1); logLik(P1); AIC(P1); BIC(P1)
+P2 <- glmmTMB(platelets ~ drug * (time + I(time^2)) + (1 + time + I(time^2)|id), PBC, poisson)
+VarCorr(P2); logLik(P2); AIC(P2); BIC(P2)
+P3 <- glmmTMB(platelets ~ drug * ns(time, 3) + (1 + ns(time, 3)|id), PBC, poisson)
+VarCorr(P3); logLik(P3); AIC(P3); BIC(P3)
+# Spline?
+# Residuals always look good
+par(mfrow=c(3,1)); plot(residuals(P1, 'pearson'));plot(residuals(P2, 'pearson'));plot(residuals(P3,'pearson'));par(mfrow=c(1,1))
+
+# Alkaline
+A1 <- glmmTMB(alkaline ~ drug * time + (1 + time|id), PBC, glmmTMB::nbinom2())
+VarCorr(A1); logLik(A1); AIC(A1); BIC(A1)
+A2 <- glmmTMB(alkaline ~ drug * (time + I(time^2)) + (1 + time + I(time^2)|id), PBC, glmmTMB::nbinom2())
+VarCorr(A2); logLik(A2); AIC(A2); BIC(A2)
+A3 <- glmmTMB(alkaline ~ drug * ns(time, 3) + (1 + ns(time, 3)|id), PBC, glmmTMB::nbinom2()) # Fails for Poisson
+VarCorr(A3); logLik(A3); AIC(A3); BIC(A3)
+par(mfrow=c(2,1)); plot(residuals(A1, 'pearson'));plot(residuals(A2, 'pearson'));par(mfrow=c(1,1))
+# Negbin fits a lot better than Poisson; residuals here point towards dropping this response altogether...
+
+# Continuous --------------------------------------------------------------
+# serBilir
+S1 <- glmmTMB(log(serBilir) ~ drug * time + (1 + time|id), PBC, gaussian)
+VarCorr(S1); logLik(S1); AIC(S1); BIC(S1)
+S2 <- glmmTMB(log(serBilir) ~ drug * (time + I(time^2)) + (1 + time + I(time^2)|id), PBC, gaussian)
+VarCorr(S2); logLik(S2); AIC(S2); BIC(S2)
+S3 <- glmmTMB(log(serBilir) ~ drug * ns(time, 3) + (1 + ns(time, 3)|id), PBC, gaussian)
+VarCorr(S3); logLik(S3); AIC(S3); BIC(S3)
+par(mfrow=c(3,1)); plot(residuals(S1, 'pearson'));plot(residuals(S2, 'pearson'));plot(residuals(S3,'pearson'));par(mfrow=c(1,1))
+# Quadratic
+
+# Aspartate aminotransfarase (log(AST))
+SG1 <- glmmTMB(AST ~ drug * time + (1 + time|id), PBC, gaussian)
+VarCorr(SG1); logLik(SG1); AIC(SG1); BIC(SG1)
+SG2 <- glmmTMB(AST ~ drug * (time + I(time^2)) + (1 + time + I(time^2)|id), PBC, gaussian)
+VarCorr(SG2); logLik(SG2); AIC(SG2); BIC(SG2)
+SG3 <- glmmTMB(AST ~ drug * ns(time, 3) + (1 + ns(time, 3)|id), PBC, gaussian)
+VarCorr(SG3); logLik(SG3); AIC(SG3); BIC(SG3)
+par(mfrow=c(3,1)); plot(residuals(SG1, 'pearson'));plot(residuals(SG2, 'pearson'));plot(residuals(SG3,'pearson'));par(mfrow=c(1,1))
+# Linear
+
+# Albumin
+# Alkaline
+A1 <- glmmTMB(albumin ~ drug * time + (1 + time|id), PBC, gaussian)
+VarCorr(A1); logLik(A1); AIC(A1); BIC(A1)
+A2 <- glmmTMB(albumin ~ drug * (time + I(time^2)) + (1 + time + I(time^2)|id), PBC, gaussian)
+VarCorr(A2); logLik(A2); AIC(A2); BIC(A2)
+A3 <- glmmTMB(albumin ~ drug * ns(time, 3) + (1 + ns(time, 3)|id), PBC, gaussian) # Fails for Poisson
+VarCorr(A3); logLik(A3); AIC(A3); BIC(A3)
+par(mfrow=c(3,1)); plot(residuals(A1, 'pearson'));plot(residuals(A2, 'pearson'));plot(residuals(A3, 'pearson'));par(mfrow=c(1,1))
+# Linear
+
+# Prothrombin
+P1 <- glmmTMB(prothrombin ~ drug * time + (1 + time|id), PBC, Gamma(link="log"))
+VarCorr(P1); logLik(P1); AIC(P1); BIC(P1)
+P2 <- glmmTMB(prothrombin ~ drug * (time + I(time^2)) + (1 + time + I(time^2)|id), PBC, gaussian)
+VarCorr(P2); logLik(P2); AIC(P2); BIC(P2)
+P3 <- glmmTMB(prothrombin ~ drug * ns(time, 3) + (1 + ns(time, 3)|id), PBC, gaussian)
+VarCorr(P3); logLik(P3); AIC(P3); BIC(P3)
+# Spline?
+# Residuals always look good
+par(mfrow=c(3,1)); plot(residuals(P1, 'pearson'));plot(residuals(P2, 'pearson'));plot(residuals(P3,'pearson'));par(mfrow=c(1,1))
--- paper-sims/Application/TrajPlot.R
+++ paper-sims/Application/TrajPlot.R
@@ -50,3 +50,39 @@ tiff('~/Downloads/PBCtrajectories.tiff',
      res = 1e3, compression = 'lzw')
 last_plot()
 dev.off()
+
+
+# Version showing survived/died -------------------------------------------
+PBC %>% 
+  pivot_longer(cols=`serBilir`:`prothrombin`, names_to='biomarker') %>% 
+  mutate(biomarker = case_when(
+    biomarker == 'serBilir' ~ "log(Serum~bilirubin)",
+    biomarker == 'albumin' ~ "Albumin",
+    biomarker == 'prothrombin' ~ '(0.1 ~ x ~ Prothrombin~time)^{-4}',
+    biomarker == 'SGOT' ~ "log(AST)",
+    biomarker == "platelets" ~ "Platelet~count",
+    biomarker == 'alkaline' ~ "Alkaline~phosphatase",
+    T ~ 'AA'
+  ),
+  tt = -1 * (survtime-time)
+  ) %>% 
+  mutate(f.biomarker = factor(biomarker, levels = c('log(Serum~bilirubin)',
+                                                    'log(AST)', 'Albumin', '(0.1 ~ x ~ Prothrombin~time)^{-4}',
+                                                    'Platelet~count', "Alkaline~phosphatase")),
+         f.surv = factor(status, levels = c(0,1),labels=c("Alive", "Dead"))) %>% 
+  filter(biomarker != 'AA') %>% 
+  ggplot(aes(x=time, y = value, group = id)) + 
+  geom_vline(xintercept = 0, colour = 'black', alpha = .25) + 
+  geom_line(alpha = .10) + 
+  geom_smooth(aes(colour=f.surv, group = f.surv), method = 'loess', formula = y~x) +
+  facet_wrap(~f.biomarker, scales = 'free', strip.position = 'left', labeller = label_parsed) + 
+  scale_y_continuous(breaks = scales::pretty_breaks(10)) +
+  scale_x_continuous(breaks = scales::pretty_breaks(10)) +
+  scale_color_brewer(palette = "Dark2") +
+  labs(y = NULL,
+       x = 'Time (years) from death (0: time of death)') + 
+  # theme_csda() + 
+  theme(strip.placement = 'outside',
+        strip.text = element_text(vjust = 1))
+  
+
